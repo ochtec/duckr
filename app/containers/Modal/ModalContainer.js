73331c536ca958462d532dce357fa81dec5e701a
Mike <mike.oconnor@ochtec.com>
2016-12-08 00:53:04 +0000

    modal completed, styling added from demo/lesson

--- app/containers/Modal/ModalContainer.js
+++ app/containers/Modal/ModalContainer.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import { Modal } from 'components'
+import * as modalActionCreators from 'redux/modules/modal'
 
-const ModalContainer = React.createClass({
-  render () {
-    return (
-      <Modal />
-    )
-  },
-})
+function mapStateToProps ({modal, users}, props) {
+  const duckTextLength = modal.duckText.length
+  return {
+    user: users[users.authedId] ? users[users.authedId].info : {},
+    duckText: modal.duckText,
+    isOpen: modal.isOpen,
+    isSubmitDisabled: duckTextLength <= 0 || duckTextLength > 140,
+  }
+}
+function mapDispatchToProps (dispatch, props) {
+  return bindActionCreators(modalActionCreators, dispatch)
+}
 
-export default ModalContainer
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Modal)
