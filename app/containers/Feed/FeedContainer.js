a6645bd07b2af9af539bdeef814dde5b049a4f49
Mike <mike.oconnor@ochtec.com>
2016-12-31 14:57:56 +0000

    duck ui complete, next is duckDetail route

--- app/containers/Feed/FeedContainer.js
+++ app/containers/Feed/FeedContainer.js
@@ -1,12 +1,50 @@
-import React from 'react'
-import { Feed }from 'components'
+import React, { PropTypes } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { Feed } from 'components'
+import * as feedActionCreators from 'redux/modules/feed'
 
 const FeedContainer = React.createClass({
+  propTypes: {
+    newDucksAvailable: PropTypes.bool.isRequired,
+    error: PropTypes.string.isRequired,
+    isFetching: PropTypes.bool.isRequired,
+    setAndHandleFeedListener: PropTypes.func.isRequired,
+    resetNewDucksAvailable: PropTypes.func.isRequired,
+    duckIds: PropTypes.array.isRequired,
+  },
+  componentDidMount () {
+    this.props.setAndHandleFeedListener()
+  },
   render () {
+    console.log(this.props)
     return (
-      <Feed />
+      <Feed
+        newDucksAvailable={this.props.newDucksAvailable}
+        error={this.props.error}
+        isFetching={this.props.isFetching}
+        resetNewDucksAvailable={this.props.resetNewDucksAvailable}
+        duckIds = {this.props.duckIds}
+      />
     )
   },
 })
 
-export default FeedContainer
+function mapStateToProps ({feed}) {
+  const { newDucksAvailable, error, isFetching, duckIds } = feed
+  return {
+    newDucksAvailable,
+    error,
+    isFetching,
+    duckIds,
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return bindActionCreators(feedActionCreators, dispatch)
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(FeedContainer)
--- app/helpers/utils.js
+++ app/helpers/utils.js
@@ -5,3 +5,18 @@ export function formatUserInfo (name, avatar, uid) {
     uid,
   }
 }
+
+export function formatDuck (text, {name, avatar, uid}) {
+  return {
+    text,
+    name,
+    avatar,
+    uid,
+    timestamp: Date.now()
+  }
+}
+
+export function formatTimestamp (timestamp) {
+  const date = new Date(timestamp)
+  return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
+}
